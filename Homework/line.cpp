4689f3b083be5f49074c806cda18e36d7b27dd84
tpcannon7 <163330846+tpcannon7@users.noreply.github.com>
2024-04-01 21:04:57 -0400

    Add files via upload

--- Homework/line.cpp
+++ Homework/line.cpp
@@ -148,6 +148,7 @@ int main()
                         }
                     }
                 }
+                cout << ch1 << " has left the line." <<endl;
                 currentLineLength--;
             }
             else
